325ff686d7d9458618f02f76328ac4830806671f
Logan Detrick <logandetrick@umail.ucsb.edu>
2024-10-09 23:21:57 -0700

    Added personalization to links

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "logandetrick",
+  name: "Logan",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "A clean, fast, and lightweight portfolio built with Next.js, Vercel, and Tailwind CSS for optimal performance. Based on Nextfolio",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/", // dont use
+  github: "https://github.com/lodetrick/portfolio",
+  instagram: "https://www.instagram.com/", // should never be used
+  linkedin: "https://www.linkedin.com/in/logan-detrick/",
+  email: "mailto:logandetrick@gmail.com",
 };
